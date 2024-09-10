3707d2be4e5256166495eddf2645b5c229ed9b3c
SUCCESS <khalid.saifullah0012@gmail.com>
2024-09-10 22:18:58 +0600

    My first upload

---
+++ src/test/java/com/microtech/gitpractice/GitPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microtech.gitpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
